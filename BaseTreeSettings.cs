3ec7dabe45a396a01ac575fb914497ae20c2415a
Sychotix <sychotixdev@gmail.com>
2018-01-23 12:10:08 -0500

    Added tooltips for settings

--- BaseTreeSettings.cs
+++ BaseTreeSettings.cs
@@ -19,13 +19,13 @@ namespace TreeRoutine
             StrictTickRate = false;
         }
 
-        [Menu("Tick Rate", 3)]
+        [Menu("Tick Rate", 3, Tooltip = "Milliseconds between every tick of plugin.")]
         public RangeNode<int> TickRate { get; set; }
 
-        [Menu("Strict Tick Rate", 4)]
+        [Menu("Strict Tick Rate", 4, Tooltip = "Enable to force a strict tick rate. This will ensure the ticks are at a constant timing, but may cause ticks to overlap as the previous tick may not have finished. Enable only if you have a reason to.")]
         public ToggleNode StrictTickRate { get; set; }
 
-        [Menu("Debug Mode", 100)]
+        [Menu("Debug Mode", 100, Tooltip = "Enables debug logging to help debug flask issues.")]
         public ToggleNode Debug { get; set; }
     }
 }
