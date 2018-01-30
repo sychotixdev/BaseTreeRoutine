4a6adfae668490f4e1f37e7c245b62444921cce1
Sychotix <sychotixdev@gmail.com>
2018-01-29 23:33:36 -0500

    Fixed a NPE with show window and set some default values for position and size

--- BaseTreeSettings.cs
+++ BaseTreeSettings.cs
@@ -16,12 +16,16 @@ namespace TreeRoutine
         public BaseTreeSettings()
         {
             Enable = false;
+
+            var centerPos = BasePlugin.API.GameController.Window.GetWindowRectangle().Center;
+            LastSettingSize = new ImGuiVector2(620, 376);
+            LastSettingPos = new ImGuiVector2(centerPos.X - LastSettingSize.X / 2, centerPos.Y - LastSettingSize.Y / 2);
         }
 
-       
+
 
         [Menu("Show ImGui Settings")]
-        public ToggleNode ShowWindow { get; set; }
+        public ToggleNode ShowWindow { get; set; } = false;
         public ImGuiVector2 LastSettingPos { get; set; }
         public ImGuiVector2 LastSettingSize { get; set; }
 
