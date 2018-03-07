23bcdd5c50405e4eb48351415a94d2fb3bd532de
Sychotix <sychotixdev@gmail.com>
2018-03-07 15:02:55 -0500

    Added a ranger render player helper
    This isn't crazy accurate, but is close enough for now.

--- DefaultBehaviors/Helpers/PlayerHelper.cs
+++ DefaultBehaviors/Helpers/PlayerHelper.cs
@@ -1,5 +1,6 @@
 ﻿using PoeHUD.Models.Enums;
 using PoeHUD.Poe.Components;
+using SharpDX;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -106,6 +107,27 @@ namespace TreeRoutine.DefaultBehaviors.Helpers
                 return null;
 
             return statValue;
+        }
+
+        public void RenderRangeFromPlayer(float range)
+        {
+            var playerPosition = new Vector3(Core.GameController.Player.GetComponent<Positioned>().WorldPos, Core.GameController.Player.Pos.Z+50);
+
+            var rightMove = new Vector3(range, 0, 0);
+            var downMove = new Vector3(0, range, 0);
+
+            var PlayerCoords = Core.GameController.Game.IngameState.Camera.WorldToScreen(playerPosition, Core.GameController.Player);
+
+            Vector3.Add(ref playerPosition, ref rightMove, out Vector3 rightPos);
+            Vector2 rightScreenPos = Core.GameController.Game.IngameState.Camera.WorldToScreen(rightPos, Core.GameController.Player);
+
+            Vector3.Add(ref playerPosition, ref downMove, out Vector3 downPos);
+            Vector2 downScreenPos = Core.GameController.Game.IngameState.Camera.WorldToScreen(rightPos, Core.GameController.Player);
+
+            var sizeX = (PlayerCoords.X - rightScreenPos.X);
+            var sizeY = (PlayerCoords.Y - rightScreenPos.Y);
+            Core.Graphics.DrawFrame(new RectangleF(PlayerCoords.X - sizeX, PlayerCoords.Y - sizeY, sizeX * 2, sizeY * 2), 2, Color.Red);
+
         }
     }
 }
