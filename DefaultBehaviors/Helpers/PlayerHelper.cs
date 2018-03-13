1d5ba109c7ba7d1c8e4a643ec3492e87042a1c51
Sychotix <sychotixdev@gmail.com>
2018-03-13 13:22:03 -0400

    Made a few changes to player helper

--- DefaultBehaviors/Helpers/PlayerHelper.cs
+++ DefaultBehaviors/Helpers/PlayerHelper.cs
@@ -109,25 +109,54 @@ namespace TreeRoutine.DefaultBehaviors.Helpers
             return statValue;
         }
 
-        public void RenderRangeFromPlayer(float range)
+        public void DrawSquareToWorld(Vector3 vector3Pos, float radius)
         {
-            var playerPosition = new Vector3(Core.GameController.Player.GetComponent<Positioned>().WorldPos, Core.GameController.Player.Pos.Z+50);
+            var rightMove = new Vector3(radius, 0, 0);
+            var downMove = new Vector3(0, radius, 0);
 
-            var rightMove = new Vector3(range, 0, 0);
-            var downMove = new Vector3(0, range, 0);
+            var PlayerCoords = Core.GameController.Game.IngameState.Camera.WorldToScreen(vector3Pos, Core.GameController.Player);
 
-            var PlayerCoords = Core.GameController.Game.IngameState.Camera.WorldToScreen(playerPosition, Core.GameController.Player);
-
-            Vector3.Add(ref playerPosition, ref rightMove, out Vector3 rightPos);
+            Vector3.Add(ref vector3Pos, ref rightMove, out Vector3 rightPos);
             Vector2 rightScreenPos = Core.GameController.Game.IngameState.Camera.WorldToScreen(rightPos, Core.GameController.Player);
 
-            Vector3.Add(ref playerPosition, ref downMove, out Vector3 downPos);
-            Vector2 downScreenPos = Core.GameController.Game.IngameState.Camera.WorldToScreen(rightPos, Core.GameController.Player);
+            Vector3.Add(ref vector3Pos, ref downMove, out Vector3 downPos);
+            Vector2 downScreenPos = Core.GameController.Game.IngameState.Camera.WorldToScreen(downPos, Core.GameController.Player);
 
             var sizeX = (PlayerCoords.X - rightScreenPos.X);
-            var sizeY = (PlayerCoords.Y - rightScreenPos.Y);
+            var sizeY = (PlayerCoords.Y - downScreenPos.Y);
             Core.Graphics.DrawFrame(new RectangleF(PlayerCoords.X - sizeX, PlayerCoords.Y - sizeY, sizeX * 2, sizeY * 2), 2, Color.Red);
+        }
 
+
+        public void DrawEllipseToWorld(Vector3 vector3Pos, int radius, int points, int lineWidth, Color color)
+        {
+            Core.Log("Testing!", 5);
+            var camera = Core.GameController.Game.IngameState.Camera;
+
+            var plottedCirclePoints = new List<Vector3>();
+            var slice = 2 * Math.PI / points;
+            for (var i = 0; i < points; i++)
+            {
+                var angle = slice * i;
+                var x = (decimal)vector3Pos.X + decimal.Multiply((decimal)radius, (decimal)Math.Cos(angle));
+                var y = (decimal)vector3Pos.Y + decimal.Multiply((decimal)radius, (decimal)Math.Sin(angle));
+                plottedCirclePoints.Add(new Vector3((float)x, (float)y, vector3Pos.Z));
+            }
+
+            for (var i = 0; i < plottedCirclePoints.Count; i++)
+            {
+                if (i >= plottedCirclePoints.Count - 1)
+                {
+                    var pointEnd1 = camera.WorldToScreen(plottedCirclePoints.Last(), Core.GameController.Player);
+                    var pointEnd2 = camera.WorldToScreen(plottedCirclePoints[0], Core.GameController.Player);
+                    Core.Graphics.DrawLine(pointEnd1, pointEnd2, lineWidth, color);
+                    return;
+                }
+
+                var point1 = camera.WorldToScreen(plottedCirclePoints[i], Core.GameController.Player);
+                var point2 = camera.WorldToScreen(plottedCirclePoints[i + 1], Core.GameController.Player);
+                Core.Graphics.DrawLine(point1, point2, lineWidth, color);
+            }
         }
     }
 }
