12b52e492aad165aec702f8473d1b1b8bf312c1a
Sychotix <sychotixdev@gmail.com>
2018-03-19 13:47:49 -0400

    Reorganized things

---
+++ src/BaseTreeSettings.cs
@@ -0,0 +1,24 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TreeRoutine
+{
+    public class BaseTreeSettings : SettingsBase
+    {
+
+        public BaseTreeSettings()
+        {
+            Enable = false;
+            Debug = false;
+        }
+
+        [Menu("Debug")]
+        public ToggleNode Debug { get; set; } = false;
+    }
+}
